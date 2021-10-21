6f9803a6b8a94531e6c4edd39eb41eabe22f7c6f
csegundo <carlossn20165@gmail.com>
2021-10-21 19:49:21 +0200

    Añadido dropdown de genero del usuario

--- android/app/src/main/java/com/dietnow/app/ucm/fdi/RegisterActivity.java
+++ android/app/src/main/java/com/dietnow/app/ucm/fdi/RegisterActivity.java
@@ -5,19 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button login;
     private Button register;
+    private Spinner genres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // elementos
         login = findViewById(R.id.registerLoginBtn);
+        genres = findViewById(R.id.registerGenre);
+
+        // acciones sobre elementos
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +34,24 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(RegisterActivity.this, R.array.genres, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genres.setAdapter(adapter);
+        genres.setOnItemSelectedListener(this);
+    }
+
+    // estos 2 metodos son de "AdapterView.OnItemSelectedListener" para el selector del genero
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String genre = parent.getItemAtPosition(position).toString();
+
+        // ahora solo muestra mensaje, luego quitarlo
+        Toast.makeText(parent.getContext(), "Género: " + genre, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
