26bc67afd592f021f88e305590116f2ccd259488
David <davidf14@ucm.es>
2021-10-24 20:31:40 +0200

    Preparing Entity User..

--- android/app/src/main/java/com/dietnow/app/ucm/fdi/model/user/User.java
+++ android/app/src/main/java/com/dietnow/app/ucm/fdi/model/user/User.java
@@ -12,5 +12,17 @@ public class User {
     private String email;
     private String name;
     private String lastname;
+    private String password;
+    private String gender;
+    private Date start_date;
+    private Double height;
+    //poner su relaccion con la tabla
+    private Double weight;
+    private String role;
+    //poner su relación con la tabla
+    private Integer steps;
+    private Boolean active;
+
+    // quedam las relaciones
 
 }
