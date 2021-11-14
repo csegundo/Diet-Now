f5e8064f2b456544271b6a8f2bf7aa9600c8c238
csegundo <carlossn20165@gmail.com>
2021-11-14 14:22:06 +0100

    Iniciando cosillas con Retrofit

---
+++ android/app/src/main/java/com/dietnow/app/ucm/fdi/apis/MailChimpService.java
@@ -0,0 +1,10 @@
+package com.dietnow.app.ucm.fdi.apis;
+
+/**
+ * Usando Retrofit se llama a la API de MailChimp para mandar el email de bienvenida
+ * campaign_id=798608
+ * https://mailchimp.com/developer/marketing/api/list-members/add-member-to-list/
+ */
+public interface MailChimpService {
+
+}
