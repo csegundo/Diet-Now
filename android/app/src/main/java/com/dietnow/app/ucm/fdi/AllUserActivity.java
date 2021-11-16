e178488587d039c55ce6f5cc509085d5ed0fc306
Vitali <vitali.sve7@gmail.com>
2021-11-16 21:33:21 +0100

    Añadida busqueda dinamica de usuarios v.0.1

--- android/app/src/main/java/com/dietnow/app/ucm/fdi/AllUserActivity.java
+++ android/app/src/main/java/com/dietnow/app/ucm/fdi/AllUserActivity.java
@@ -1,4 +1,106 @@
 package com.dietnow.app.ucm.fdi;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dietnow.app.ucm.fdi.model.user.User;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/*Codigo original que metia valores de manera dinamica
+ * https://www.tutorialspoint.com/how-to-dynamically-update-a-listview-in-android
+ * */
+
+
+public class AllUserActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button;
+    private ListView listView;
+    private DataSnapshot userList;
+    private ArrayList<String> list = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private FirebaseAuth auth;
+    private DatabaseReference db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_user);
+        db       = FirebaseDatabase.getInstance(MainActivity.FIREBASE_DB_URL).getReference();
+        fillList();
+        button = findViewById(R.id.btnAdd);
+        listView = findViewById(R.id.listView);
+        editText = findViewById(R.id.editText);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //La idea es que una vez se hace clic la linea de abajo filtra y solo contiene
+                //a los usuarios cuyos nombres se parezcan a lo introducido
+                List<String> filteredList = Lists.newArrayList(Collections2.filter(
+                        list, Predicates.containsPattern(editText.getText().toString())));
+
+                editText.setText("");
+                adapter.notifyDataSetChanged();
+            }
+        };
+        button.setOnClickListener(onClickListener);
+        listView.setAdapter(adapter);
+    }
+    private void fillList(){
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<String> names= new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.child("users").getChildren()) {
+                    String name = ds.child("name").getValue(String.class);
+                    names.add(name);
+                    Log.d("TAG", name);
+                }
+                asignResult(names);
+                //En este punto la lista names contiene los nombres de los usuarios registrados pero no se
+                //como sacarla de aqui y mostrarla por pantalla --> es necesario sacarla de aqui y meterla en la linea
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG", "UserPost:onCancelled", databaseError.toException());
+            }
+        };
+        db.addValueEventListener(postListener);
+    }
+    private void asignResult (ArrayList<String> query){
+        list = query;
+    }
+
+
+
+}
+
+
+
+
+/*package com.dietnow.app.ucm.fdi;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,4 +131,14 @@ public class AllUserActivity extends AppCompatActivity {
         tv.setTextSize(30);
 
     }
-}
+}*/
+
+
+
+
+
+
+
+
+
+
