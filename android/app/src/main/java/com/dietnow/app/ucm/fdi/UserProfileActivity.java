9ce33475ca5d59fa45bfbd4a78d11efbc362ef62
csegundo <carlossn20165@gmail.com>
2021-11-18 20:05:50 +0100

    Subida de imagenes DONE. Queda bajarlas para mostrarlas en el perfil

--- android/app/src/main/java/com/dietnow/app/ucm/fdi/UserProfileActivity.java
+++ android/app/src/main/java/com/dietnow/app/ucm/fdi/UserProfileActivity.java
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +20,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.*;
 import com.dietnow.app.ucm.fdi.model.user.*;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
@@ -37,8 +48,10 @@ public class UserProfileActivity extends AppCompatActivity {
     private TextView name, age;
     private Button settings;
     private ImageView image;
+    private Uri filePath;
     private Button delete, change;
-    private DatabaseReference mDatabase;
+
+    private DatabaseReference db;
     private FirebaseAuth auth;
     private FirebaseStorage storage;
     private StorageReference storageRef, imagesRef;
@@ -57,12 +70,16 @@ public class UserProfileActivity extends AppCompatActivity {
 
         // inicializar Google Firebase
         auth       = FirebaseAuth.getInstance();
-        mDatabase  = FirebaseDatabase.getInstance(MainActivity.FIREBASE_DB_URL).getReference();
-        storage    = FirebaseStorage.getInstance(); // se crea la instancia
-        storageRef = storage.getReference(); // crear una referencia del almacenamiento
-        imagesRef  = storageRef.child("images"); // referencia exclusivamente para imagenes
+        db         = FirebaseDatabase.getInstance(MainActivity.FIREBASE_DB_URL).getReference();
 
-        // llamada a Firebase para obtener la info del usuario logueado
+        /*
+         * Para acceder a imagenes es con imagesRef.child("fileName")
+         * Propiedades de las referencias: getPath(), getName() y getBucket()
+         */
+        storageRef = FirebaseStorage.getInstance().getReference(); // crear una instancia a la referencia del almacenamiento
+        imagesRef  = storageRef.child("images"); // referencia exclusivamente para imagenes (nivel mas bajo)
+
+        // llamada a Firebase para obtener la info del usuario logueado y actualizar la vista
         FirebaseUser currentUser = auth.getCurrentUser();
         ValueEventListener profileListener = new ValueEventListener() {
             @Override
@@ -72,11 +89,28 @@ public class UserProfileActivity extends AppCompatActivity {
                 name.setText(user.getName().trim() + " " + user.getLastname().trim());
                 age.setText(user.getAge() + " " + age.getText().toString());
 
-                // TODO cambiar para que en el usuario se almacene la imagen de su perfil
-                Boolean userHasImage = false;
-                if(!userHasImage){
-                    image.setImageResource(R.drawable.ic_person_128_black);
-                }
+                // TODO buscar la imagen de perfil del usuario en Fireabse Storage
+                String fileName = "profile_" + currentUser.getUid() + ".jpg";
+                storageRef.child("images/" + fileName).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Toast.makeText(getApplicationContext(), uri.toString(), Toast.LENGTH_LONG).show();
+                        try {
+                            /*InputStream imgStream = new java.net.URL(uri.toString()).openStream();
+                            Bitmap bitmap = BitmapFactory.decodeStream(imgStream);
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            image.setImageBitmap(bitmap);*/
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            image.setImageResource(R.drawable.ic_person_128_black); // imagen predeterminada
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        image.setImageResource(R.drawable.ic_person_128_black); // imagen predeterminada
+                    }
+                });
             }
 
             @Override
@@ -84,10 +118,9 @@ public class UserProfileActivity extends AppCompatActivity {
                 Log.w("UserProfile", "UserProfile:onCancelled", databaseError.toException());
             }
         };
-        mDatabase.addValueEventListener(profileListener);
+        db.addValueEventListener(profileListener);
 
         // Cambiar la imagen del usuario: gallery/docs
-        // https://stackoverflow.com/questions/5309190/android-pick-images-from-gallery
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,7 +128,7 @@ public class UserProfileActivity extends AppCompatActivity {
             }
         });
 
-        // Acciones de los componentes
+        // Acciones del perfil
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,7 +136,7 @@ public class UserProfileActivity extends AppCompatActivity {
             }
         });
 
-        // Acciones de los componentes
+        // Borrar perfil del usuario
         delete = findViewById(R.id.deleteProfile);
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,18 +152,59 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
-            Log.d("IMAGEEEEEEEEEEEEEEEEN", String.valueOf(requestCode) + " " + String.valueOf(resultCode));
-
             if(data == null){
                 return;
             }
-
             try{
-                InputStream inputStream = getApplicationContext().getContentResolver().openInputStream(data.getData());
-                // TODO convertir inputStream en Image y guardarlo en (?) https://stackoverflow.com/questions/5729236/whats-the-best-way-to-store-images-in-android
+                // TODO proceso explicativo https://www.youtube.com/watch?v=ZmgncLHk_s4
+                filePath = data.getData();
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                image.setImageBitmap(bitmap); // actualizar la imagen directamente a la vista
+                uploadImage(); // subir la imagen a Google Firebase Storage
             } catch(Exception e){
                 e.printStackTrace();
             }
+        }
+    }
+
+    /*
+    * Sube una imagen a Google Firebase Storage
+    * Si ya tiene subida una imagen y se sube otra se sobreescribe
+    */
+    private void uploadImage(){
+        if(filePath != null){
+            ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setTitle(getResources().getString(R.string.uploading));
+            dialog.show();
+
+            FirebaseUser currentUser = auth.getCurrentUser();
+
+            // Se crea una referencia a la ruta de acceso completa del archivo
+            String fileName = "profile_" + currentUser.getUid() + ".jpg";
+            StorageReference userProfile = storageRef.child("images/" + fileName);
+
+            // Se sube la imagen
+            userProfile.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    dialog.dismiss();
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.uploaded_final), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    e.printStackTrace();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                    // trackear el proceso de subida de la imagen
+                    Double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                    dialog.setMessage(progress.toString() + "% " + getResources().getString(R.string.uploaded_progress));
+                }
+            });
+        } else{
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.select_image_error), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -166,7 +240,7 @@ public class UserProfileActivity extends AppCompatActivity {
                     User user = dataSnapshot.child("users").child(userAuth.getUid()).getValue(User.class);
                     user.setActive(false);
                     Map<String, Object> userValues = user.toMap();
-                    mDatabase.child("users").child(userAuth.getUid()).updateChildren(userValues);
+                    db.child("users").child(userAuth.getUid()).updateChildren(userValues);
 
                     // redirige a la página principal
                     // auth.signOut();
@@ -186,7 +260,7 @@ public class UserProfileActivity extends AppCompatActivity {
                 Log.w("TAG", "UserPost:onCancelled", databaseError.toException());
             }
         };
-        mDatabase.addValueEventListener(postListener);
+        db.addValueEventListener(postListener);
     }
 
 }
