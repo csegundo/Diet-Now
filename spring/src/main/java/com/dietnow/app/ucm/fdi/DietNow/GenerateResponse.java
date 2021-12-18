dba6b711111c8b3fe9e6b720f480909ab93e292d
csegundo <carlossn20165@gmail.com>
2021-12-18 13:16:22 +0100

    Test Spring

--- spring/src/main/java/com/dietnow/app/ucm/fdi/DietNow/GenerateResponse.java
+++ spring/src/main/java/com/dietnow/app/ucm/fdi/DietNow/GenerateResponse.java
@@ -1,5 +1,8 @@
 package com.dietnow.app.ucm.fdi.DietNow;
 
+/**
+ * Genera un String JSON para devolverlo a Retrofit
+ */
 public class GenerateResponse {
     public static String generateJSON(Boolean success, String message){
         return "{\"success\":" + success + ",\"message\":\"" + message + "\"}";
